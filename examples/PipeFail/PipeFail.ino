d0d10f7429c1f75953d5adcc939ddfb77cd25326
Brian Park <brian@xparks.net>
2021-03-21 15:45:39 -0700

    examples/PipeFail: Add example that reproduces #2

---
+++ examples/PipeFail/PipeFail.ino
@@ -0,0 +1,43 @@
+/*
+ * A program to reproduce https://github.com/bxparks/EpoxyDuino/issues/2, where
+ * piping to a pager program like 'less' or 'more' fails if too much data is
+ * written to the SERIAL_PORT_MONITOR.
+ *
+ * To run:
+ *
+ * $ make clean
+ * $ make
+ * $ ./PipeFail.out | less # this fails
+ * $ ./PipeFail.out < /dev/null | less # this works
+ */
+
+#include <Arduino.h>
+
+// Fails for NUM_LINES >= 274 on Ubuntu 20.04.
+const int NUM_LINES = 300;
+const char LINE[] = "Reproduce https://github.com/bxparks/EpoxyDuino/issues/2";
+
+void setup() {
+#if ! defined(EPOXY_DUINO)
+  delay(1000); // some boards reboot twice
+#endif
+  SERIAL_PORT_MONITOR.begin(115200);
+  while (!SERIAL_PORT_MONITOR); // For Leonardo/Micro
+
+  size_t totalCharacters = 0;
+  for (int i = 0; i < NUM_LINES; i++) {
+    SERIAL_PORT_MONITOR.print(i);
+    SERIAL_PORT_MONITOR.print(" (");
+    SERIAL_PORT_MONITOR.print(totalCharacters);
+    SERIAL_PORT_MONITOR.print("): ");
+    SERIAL_PORT_MONITOR.println(LINE);
+    totalCharacters += sizeof(LINE) + 10; // line is variable, so take a guess
+  }
+
+#if defined(EPOXY_DUINO)
+  exit(0);
+#endif
+}
+
+void loop() {
+}
