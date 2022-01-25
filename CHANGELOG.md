26395cb6b52edfe168e6f8ee672d3298f5e74dc2
Brian Park <brian@xparks.net>
2022-01-25 09:44:54 -0800

    StdioSerial.h: Add 'using Print::write' (replaces #56)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog
 
 * Unreleased
+    * Add a `using Print::write` statement in `StdioSerial.h` to
+      pull in all other overloaded `write()` methods from the parent `Print`
+      class.
 * 1.2.1 (2022-01-10)
     * Add `strncasecmp_P()` to `pgmspace.h`. See
       [PR#52](https://github.com/bxparks/EpoxyDuino/pull/52).
--- cores/epoxy/StdioSerial.h
+++ cores/epoxy/StdioSerial.h
@@ -19,14 +19,23 @@ class StdioSerial: public Stream {
 
     size_t write(uint8_t c) override;
 
+    // Pull in all other overloaded versions of the write() function from the
+    // Print parent class. This is required because when we override one version
+    // of write() above, C++ performs a static binding to the write() function
+    // in the current class and doesn't bother searching the parent classes for
+    // any other overloaded function that it could bind to. (30 years of C++ and
+    // I still get shot with C++ footguns like this. I have no idea what happens
+    // if the Stream class overloaded the write() function.)
+    using Print::write;
+
     operator bool() { return true; }
 
     int available() override;
 
     int read() override;
-	
+
     int peek() override;
-    
+
   private:
     int bufch;
 };
