2e60390ed38adde63dc8ce69ad0309507c26f449
Matt Kouba <matt.kouba@gmail.com>
2023-08-20 14:18:29 -0400

    Add SPI tests
    Added tests to ensure SPI Mocking functionality works as expected

---
+++ tests/SPIMockTest/SPIMockTest.ino
@@ -0,0 +1,146 @@
+#line 2 "SPIMockTest"
+
+#include <Arduino.h>
+#include <AUnit.h>
+#include <SPI.h>
+
+using aunit::TestRunner;
+
+#define csPin 10
+
+/* Macros to make starting and ending easier*/
+#define SPI_START digitalWrite(csPin, LOW)
+#define SPI_END digitalWrite(csPin, HIGH)
+
+/*Test writing 8 bits with SPI*/
+test(SPIMockTest, write8)
+{
+
+	SPI_START;
+
+	/*Write 8 bits*/
+	SPI.transfer(0x8F);
+
+	SPI_END;
+
+	/*Get value that we wrote*/
+	uint16_t writtenValue = SPI.transferWriteValue();
+
+	/*Make sure we actually wrote 0x8F*/
+	assertEqual(0x8F, writtenValue);
+}
+
+/*Test writing 16 bits with SPI*/
+test(SPIMockTest, write16)
+{
+
+	SPI_START;
+
+	/*Write 16 bits*/
+	SPI.transfer16(0x8F8F);
+
+	SPI_END;
+
+	/*Get value that we wrote*/
+	uint16_t writtenValue = SPI.transferWriteValue();
+
+	/*Make sure we actually wrote 0x8F*/
+	assertEqual(0x8F8F, writtenValue);
+}
+
+/*Test reading a given 8 bits with SPI*/
+test(SPIMockTest, read8)
+{
+
+	/*Setting expected read value*/
+	SPI.transferReturnValue(0x5F);
+
+	SPI_START;
+
+	/*Write 8 bits so we can get a read 8 bits*/
+	uint16_t readValue = SPI.transfer(0x8F);
+
+	SPI_END;
+
+	/*Make sure we actually read 0x8F*/
+	assertEqual(0x5F, readValue);
+}
+
+/*Test reading a given 16 bits with SPI*/
+test(SPIMockTest, read16)
+{
+
+	/*Setting expected read value*/
+	SPI.transferReturnValue(0x5F5F);
+
+	SPI_START;
+
+	/*Write 8 bits so we can get a read 8 bits*/
+	uint16_t readValue = SPI.transfer16(0x8F8F);
+
+	SPI_END;
+
+	/*Make sure we actually read 0x8F*/
+	assertEqual(0x5F5F, readValue);
+}
+
+/*Test that we can both read and write values at the same time
+  8 bits at a time*/
+test(SPIMockTest, readWrite8)
+{
+
+	/*Setting expected read value*/
+	SPI.transferReturnValue(0xFF);
+
+	SPI_START;
+
+	/*Write 8 bits*/
+	uint16_t readValue = SPI.transfer(0x88);
+
+	SPI_END;
+
+	/*get value we wrote*/
+	uint16_t writtenValue = SPI.transferWriteValue();
+	/*Ensure we wrote read and wrote correct 8 bits*/
+	assertEqual(0xFF, readValue);
+	assertEqual(0x88, writtenValue);
+}
+
+test(SPIMockTest, readWrite16)
+{
+
+	/*Setting expected read value*/
+	SPI.transferReturnValue(0xFF88);
+
+	SPI_START;
+
+	/*Write 8 bits*/
+	uint16_t readValue = SPI.transfer16(0x88FF);
+
+	SPI_END;
+
+	/*get value we wrote*/
+	uint16_t writtenValue = SPI.transferWriteValue();
+	/*Ensure we wrote read and wrote correct 8 bits*/
+	assertEqual(0xFF88, readValue);
+	assertEqual(0x88FF, writtenValue);
+}
+
+void setup()
+{
+
+#if !defined(EPOXY_DUINO)
+	delay(1000); // wait to prevent garbage on SERIAL_PORT_MONITOR
+#endif
+
+	SERIAL_PORT_MONITOR.begin(115200);
+	while (!SERIAL_PORT_MONITOR)
+		; // needed for Leonardo/Micro
+
+	SPI.begin();
+}
+
+void loop()
+{
+	TestRunner::run();
+}
